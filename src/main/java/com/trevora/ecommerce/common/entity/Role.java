e83c5a44fbc617ce46ffc890beaa72a1ab2d4075
Anzyll <anzil2003m@gmail.com>
2026-02-25 12:31:18 +0530

    test: integration test for order lifecycle success

--- src/main/java/com/trevora/ecommerce/common/entity/Role.java
+++ src/main/java/com/trevora/ecommerce/common/entity/Role.java
@@ -1,11 +1,12 @@
 package com.trevora.ecommerce.common.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter@Setter@NoArgsConstructor
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Role {
---
+++ src/test/java/com/trevora/ecommerce/Integration/OrderPlacementIntegrationTest.java
@@ -0,0 +1,127 @@
+package com.trevora.ecommerce.Integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trevora.ecommerce.cart.entity.Cart;
+import com.trevora.ecommerce.cart.entity.CartItem;
+import com.trevora.ecommerce.cart.repository.CartItemRepository;
+import com.trevora.ecommerce.cart.repository.CartRepository;
+import com.trevora.ecommerce.common.entity.Role;
+import com.trevora.ecommerce.common.entity.User;
+import com.trevora.ecommerce.common.enums.CartStatus;
+import com.trevora.ecommerce.common.repository.RoleRepository;
+import com.trevora.ecommerce.common.repository.UserRepository;
+import com.trevora.ecommerce.order.dto.CheckoutRequestDto;
+import com.trevora.ecommerce.order.entity.Order;
+import com.trevora.ecommerce.order.repository.OrderRepository;
+import com.trevora.ecommerce.product.entity.Activity;
+import com.trevora.ecommerce.product.entity.Category;
+import com.trevora.ecommerce.product.entity.Product;
+import com.trevora.ecommerce.product.repository.ActivityRepository;
+import com.trevora.ecommerce.product.repository.CategoryRepository;
+import com.trevora.ecommerce.product.repository.ProductRepository;
+import com.trevora.ecommerce.security.CustomUserDetails;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderPlacementIntegrationTest extends BaseIntegrationTest{
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private ActivityRepository activityRepository;
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CartItemRepository cartItemRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    Product product;
+    Category category;
+    Activity activity;
+
+    @BeforeEach
+    void setup() {
+        category = new Category();
+        category.setName("category");
+        categoryRepository.save(category);
+        activity=new Activity();
+        activity.setName("activity");
+        activityRepository.save(activity);
+        product = new Product();
+        product.setName("iPhone");
+        product.setCategory(category);
+        product.setActivity(activity);
+        product.setPrice(BigDecimal.valueOf(50000));
+        product.setStock(10);
+        productRepository.save(product);
+
+    }
+
+    @Test
+    void shouldPlaceOrderAndReduceStockSuccessfully() throws Exception {
+        Role role = roleRepository.findByTitle("ROLE_USER")
+                .orElseGet(() -> {
+                    Role r = new Role();
+                    r.setTitle("ROLE_USER");
+                    return roleRepository.save(r);
+                });
+
+        User testUser = new User();
+        testUser.setEmail("test@gmail.com");
+        testUser.setRole(role);
+        testUser.setPassword("0000");
+        userRepository.save(testUser);
+        Cart cart = new Cart();
+        cart.setStatus(CartStatus.ACTIVE);
+        cart.setCartItem(new ArrayList<>());
+        cart.setUser(testUser);
+
+        cartRepository.save(cart);
+        CartItem cartItem = new CartItem();
+        cartItem.setProduct(product);
+        cartItem.setQuantity(2);
+        cartItem.setCart(cart);
+        cartItemRepository.save(cartItem);
+        cart.getCartItem().add(cartItem);
+        CheckoutRequestDto requestDto = new CheckoutRequestDto("kerala");
+        mockMvc.perform(post("/api/orders/checkout")
+                        .with(csrf())
+                        .with(user(new CustomUserDetails(testUser)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.orderId").exists());
+
+        List<Order> orders = orderRepository.findAll();
+        assertEquals(1,orders.size());
+    }
+
+
+}
